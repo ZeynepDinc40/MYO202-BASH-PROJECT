a895c6bdf78e5ffcf27fab3c7ee98b8c5c20b54f
Zeynep Feyza <zeyneepdinc40@gmail.com>
2026-05-31 16:33:29 +0300

    final version

--- main.sh
+++ main.sh
@@ -7,30 +7,30 @@
 # https://credsverse.com/credentials/3cf102d1-e682-41ed-84c7-6f4c33f8c5db
 
 
-echo "Program basladi"
+echo "program basladi"
 
 echo "Tarih Bilgisi:" > report.log
-date >> report.log
+date -Iseconds >> report.log
 
 echo "" >> report.log
 echo "Islemci:" >> report.log
-powershell "Get-CimInstance Win32_Processor | Select Name" >> report.log
+wmic cpu get name >> report.log 2>/dev/null || powershell "Get-CimInstance Win32_Processor | Select Name" >> report.log
 
 echo "" >> report.log
 echo "RAM:" >> report.log
-powershell "Get-CimInstance Win32_PhysicalMemory | Select Capacity" >> report.log
+wmic memorychip get capacity >> report.log 2>/dev/null || powershell "Get-CimInstance Win32_PhysicalMemory | Select Capacity" >> report.log
 
 echo "" >> report.log
 echo "Anakart:" >> report.log
-powershell "Get-CimInstance Win32_BaseBoard | Select Product" >> report.log
+wmic baseboard get product >> report.log 2>/dev/null || powershell "Get-CimInstance Win32_BaseBoard | Select Product" >> report.log
 
 echo "" >> report.log
 echo "UUID:" >> report.log
-powershell "Get-CimInstance Win32_ComputerSystemProduct | Select UUID" >> report.log
+wmic csproduct get uuid >> report.log 2>/dev/null || powershell "Get-CimInstance Win32_ComputerSystemProduct | Select UUID" >> report.log
 
 echo "" >> report.log
 echo "Disk:" >> report.log
-powershell "Get-CimInstance Win32_DiskDrive | Select SerialNumber" >> report.log
+wmic diskdrive get serialnumber >> report.log 2>/dev/null || powershell "Get-CimInstance Win32_DiskDrive | Select SerialNumber" >> report.log
 
 echo "" >> report.log
 echo "MAC:" >> report.log
@@ -42,4 +42,4 @@ gpg --batch --yes --passphrase "$PAROLA" -c --cipher-algo AES256 report.log
 
 rm report.log
 
-echo "Bitti"
+echo "bitti"
